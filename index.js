8bd62c82d3708a266a2373e5130b2ecd79738b7f
ぱる！ <80621852+Parunanodesu@users.noreply.github.com>
2021-05-23 17:02:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+//Web Page
+const http = require('http');
+http.createServer(function (req, res) {
+  res.write("無事成功しました。");
+  res.end();
+}).listen(8080);
 
+//定義系
+const Discord = require('discord.js');
+const fetch = require("node-fetch");
+const client = new Discord.Client();
+const Client = new Discord.Client();
+const token = process.env.DISCORD_BOT_TOKEN;
+const prefix = "k#"
+
+//起動設定
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(client.guilds.cache.size + "serversに導入済み | 暇を持て余した神々の遊び", { type: "COMPETING" },{ status: "online" });
+});
+
+//Login
+client.login(token);
+
+//メッセージ対応
+client.on('message', async message =>{
+  if (message.author.id == client.user.id || message.author.bot){
+    return;
+  }
+if (message.content === "にゃーん"){
+    message.reply("にゃーん")
+      .then(message => console.log("Sent message: " + "にゃーん"))
+      .catch(console.error);
+    return;
+  }
+if (message.content === prefix + "ping") {
+    message.channel.send(":ping_pong:",
+      {embed: {
+         title: ":ping_pong:Pong!",
+         description: "ただいまのPing値は" + client.ws.ping + "msです。"
+      }});
+  };
